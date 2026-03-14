3051ebeabcfbbb7d0ec10bd7dbc1e9304562bf0c
Dominik <dominik.hoch.1989@freenet.de>
2026-03-14 12:10:19 +0100

    changed formating for output

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ def main():
     print("Hello from raspi-temps!")
 
     vcgm = Vcgencmd()
-    print(vcgm.version())
-    print(vcgm.measure_temp())
-
+    print("Temperature: " + vcgm.measure_temp() + "°C")
 
 if __name__ == "__main__":
     main()
