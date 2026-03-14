7436f560d578419ecd640410c19be7fb17dc720a
Dominik <dominik.hoch.1989@freenet.de>
2026-03-14 12:15:22 +0100

    changed to f-string

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     print("Hello from raspi-temps!")
 
     vcgm = Vcgencmd()
-    print("Temperature: " + vcgm.measure_temp() + "°C")
+    print(f"Temperature: {vcgm.measure_temp()}°C")
 
 if __name__ == "__main__":
     main()
