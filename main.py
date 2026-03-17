6e452b01ce479b2418bb62bc224d41fffe1a3655
Dominik Hoch <dominik.hoch.1989@freenet.de>
2026-03-17 20:21:24 +0100

    added mqtt-publish

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from vcgencmd import Vcgencmd
 import warnings
+from time import sleep
+import paho.mqtt.client as mqtt
+
+NAME = "nextpi"
 
 def main():
     warnings.filterwarnings("ignore", category=SyntaxWarning, module="vcgencmd")
     print("Hello from raspi-temps!")
 
     vcgm = Vcgencmd()
-    print(f"Temperature: {vcgm.measure_temp()}°C")
+    client = mqtt.Client()
+    client.connect("192.168.178.100", 1883)
+
+    while True:
+        temp = vcgm.measure_temp()
+        temp_str = f"Temperature: {vcgm.measure_temp()}°C"
+        print(temp_str)
+        client.publish(f"pi/temps/{NAME}", temp_str)
 
 if __name__ == "__main__":
     main()
